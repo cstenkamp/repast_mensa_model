6fdb59d86bc9c543a6d9711efd1a7ec19437183f
cstenkamp <529-cstenkamp@users.noreply.gitlab.informatik.uni-osnabrueck.de>
2020-01-06 17:46:11 +0100

    first commit

---
+++ src/mensamodell/basestudent.java
@@ -0,0 +1,12 @@
+package mensamodell;
+
+import repast.simphony.engine.schedule.ScheduledMethod;
+
+public class basestudent {
+
+	@ScheduledMethod(start = 0, interval = 1, shuffle = true)
+	public void step() {
+		//pass
+	}
+		
+}
---
+++ src/mensamodell/modelbuilder.java
@@ -0,0 +1,30 @@
+package mensamodell;
+
+import repast.simphony.context.Context;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.space.continuous.*;
+import repast.simphony.random.RandomHelper;
+
+public class modelbuilder implements ContextBuilder<Object>{
+
+	@Override
+	public Context build(Context<Object> context) {
+		ContinuousSpaceFactory spaceFact = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null);
+		
+		ContinuousSpace<Object> space = spaceFact.createContinuousSpace("space", context, new SimpleCartesianAdder<Object>(), new WrapAroundBorders(), 10, 10);
+
+		for (int i = 0; i < 10; i++) {
+			basestudent stud = new basestudent();
+			context.add(stud);
+
+			double x = RandomHelper.nextIntFromTo(0, 10); 
+			double y = RandomHelper.nextIntFromTo(0, 10); 
+			space.moveTo(stud, x, y);	
+		}
+		
+		return context;
+	}
+
+}
